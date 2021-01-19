877f55eb0c05d27d51727be67abf3e706d1740f0
DESKTOP-941CPTS\Quang Tuan <tr.ngochai23@gmail.com>
2021-01-20 00:02:07 +0700

    fix oop

--- BlasterMaster/Enemies.cpp
+++ BlasterMaster/Enemies.cpp
@@ -7,3 +7,17 @@ CEnemies::CEnemies(float x, float y)
 void CEnemies::RemoveHealth()
 {
 }
+
+void CEnemies::Render()
+{
+	animation_set->at(ani)->Render(x, y);
+	RenderBoundingBox();
+}
+
+void CEnemies::GetBoundingBox(float& left, float& top, float& right, float& bottom, int bbox_width, int bbox_height)
+{
+	left = x;
+	top = y;
+	right = x + bbox_width;
+	bottom = y + bbox_height;
+}
--- BlasterMaster/Enemies.h
+++ BlasterMaster/Enemies.h
@@ -6,10 +6,12 @@ class CEnemies : public CGameObject
 	int health;
 
 public:
+	int ani;
 	CEnemies(float x = 0.0f, float y = 0.0f);
 	int GetHealth() { return this->health; }
-
 	virtual void RemoveHealth();
 	virtual void SetHealth(int health) { this->health = health; }
+	virtual void GetBoundingBox(float& left, float& top, float& right, float& bottom, int bbox_width, int bbox_height);
+	void Render();
 };
 
